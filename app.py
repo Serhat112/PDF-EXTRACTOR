31818585fbef30e23f7734182ebeefc29911cdc4
Serhat112 <serhatakincirak@gmail.com>
2026-03-05 18:27:49 +0200

    Initial commit: PDF extractor code

---
+++ app.py
@@ -0,0 +1,145 @@
+import os
+
+os.environ["TCL_LIBRARY"] = r"C:\Users\serha\AppData\Local\Programs\Python\Python313\tcl\tcl8.6"
+os.environ["TK_LIBRARY"] = r"C:\Users\serha\AppData\Local\Programs\Python\Python313\tcl\tk8.6"
+
+import customtkinter as ctk
+from tkinterdnd2 import TkinterDnD, DND_FILES
+from pypdf import PdfReader, PdfWriter
+from tkinter import filedialog
+
+selected_file = None
+total_pages = 0
+
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("blue")
+
+
+class App(TkinterDnD.Tk):
+
+    def __init__(self):
+        super().__init__()
+
+        self.title("PDF Page Extractor")
+        self.geometry("500x400")
+
+        self.label = ctk.CTkLabel(
+            self,
+            text="PDF Page Extractor",
+            font=("Arial", 20),
+            text_color="black"
+        )
+
+        self.label.pack(pady=30)
+
+        self.drop_area = ctk.CTkFrame(
+            self,
+            width=400,
+            height=120,
+            fg_color="#1f1f1f",
+            border_width=2,
+            border_color="#444444"
+        )
+        self.drop_area.pack(pady=10)
+
+        self.drop_label = ctk.CTkLabel(
+            self.drop_area,
+            text="Drag & Drop PDF Here",
+            text_color="red",
+            font=("Arial", 16)
+        )
+        self.drop_label.place(relx=0.5, rely=0.5, anchor="center")
+
+        self.drop_label.place(relx=0.5, rely=0.5, anchor="center")
+
+        self.drop_area.drop_target_register(DND_FILES)
+        self.drop_area.dnd_bind('<<Drop>>', self.drop)
+
+        self.file_label = ctk.CTkLabel(self, text="", text_color="white")
+        self.file_label.pack()
+
+        self.page_label = ctk.CTkLabel(self, text="", text_color="white")
+        self.page_label.pack()
+
+        self.calc_label = ctk.CTkLabel(self, text="", text_color="lightgreen")
+        self.calc_label.pack()
+
+        self.start_entry = ctk.CTkEntry(
+            self,
+            placeholder_text="Start Page",
+            text_color="white",
+            fg_color="#2b2b2b",
+            border_color="#555555"
+        )
+        self.start_entry.pack(pady=10)
+
+        self.end_entry = ctk.CTkEntry(
+            self,
+            placeholder_text="End Page",
+            text_color="white",
+            fg_color="#2b2b2b",
+            border_color="#555555"
+        )
+        self.end_entry.pack(pady=10)
+
+        self.start_entry.bind("<KeyRelease>", self.calculate_pages)
+        self.end_entry.bind("<KeyRelease>", self.calculate_pages)
+
+        self.extract_button = ctk.CTkButton(self, text="Extract PDF", command=self.extract)
+        self.extract_button.pack(pady=20)
+
+    def drop(self, event):
+        global selected_file, total_pages
+
+        selected_file = event.data.strip("{}")
+
+        reader = PdfReader(selected_file)
+        total_pages = len(reader.pages)
+
+        filename = os.path.basename(selected_file)
+
+        self.file_label.configure(text=f"Loaded: {filename}", text_color="black")
+        self.page_label.configure(text=f"Total Pages: {total_pages}", text_color="black")
+
+    def calculate_pages(self, event=None):
+        try:
+            start = int(self.start_entry.get())
+            end = int(self.end_entry.get())
+
+            pages = end - start + 1
+
+            self.calc_label.configure(text=f"Extracting {pages} pages", text_color="black")
+
+        except:
+            self.calc_label.configure(text="")
+
+    def extract(self):
+        try:
+            start = int(self.start_entry.get())
+            end = int(self.end_entry.get())
+
+            reader = PdfReader(selected_file)
+            writer = PdfWriter()
+
+            for i in range(start - 1, end):
+                writer.add_page(reader.pages[i])
+
+            output = filedialog.asksaveasfilename(
+                defaultextension=".pdf",
+                filetypes=[("PDF files", "*.pdf")],
+            )
+
+            if not output:
+                return
+
+            with open(output, "wb") as f:
+                writer.write(f)
+
+            self.calc_label.configure(text="PDF created: extracted.pdf", text_color="black")
+
+        except Exception as e:
+            self.calc_label.configure(text=str(e))
+
+
+app = App()
+app.mainloop()
